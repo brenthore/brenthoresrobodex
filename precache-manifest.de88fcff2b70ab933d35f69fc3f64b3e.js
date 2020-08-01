20d5c98aacb50e2be4d549b081f325cc42d2673a
brenthore <franzisbugna@yahoo.com>
2020-08-01 14:38:54 +0800

    Updates

---
+++ precache-manifest.de88fcff2b70ab933d35f69fc3f64b3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a5eee39f670c74587d4bba4accfad8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d83da5dbbb62bfa35f9a",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "476ec06a8b4f90537fdb",
+    "url": "/static/css/main.cb957505.chunk.css"
+  },
+  {
+    "revision": "d83da5dbbb62bfa35f9a",
+    "url": "/static/js/2.a50280bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a50280bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "476ec06a8b4f90537fdb",
+    "url": "/static/js/main.54fd7562.chunk.js"
+  },
+  {
+    "revision": "170e03914560ba2b35ae",
+    "url": "/static/js/runtime-main.e44f51db.js"
+  },
+  {
+    "revision": "d5acacaf00b5cfadf82d447d7b085045",
+    "url": "/static/media/Blanka-Regular.d5acacaf.woff"
+  }
+]);
